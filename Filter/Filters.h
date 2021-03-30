178f179c3aed468a8c9d6340e9db9ec6dfbdc1ab
Gekata-2 <60921872+Gekata-2@users.noreply.github.com>
2021-03-30 10:31:37 +0300

    Update Filters.h

--- Filter/Filters.h
+++ Filter/Filters.h
@@ -14,3 +14,44 @@ class InvertFilter :public Filter
 	QColor calcNewPixelColor(const QImage& img, int x, int y) const override;
 };
 
+
+class Kernel
+{
+protected:
+	std::unique_ptr<float[]> data;
+
+	std::size_t radius;
+
+
+	std::size_t getLen() const
+	{
+		return getSize() * getSize();
+	}
+
+public:
+	Kernel(std::size_t radius) :radius(radius)
+	{
+		data = std::make_unique<float[]>(getLen());
+	}
+
+	Kernel(const Kernel& other) : Kernel(other.radius)
+	{
+		std::copy(other.data.get(), other.data.get() + getLen(), data.get());
+	}
+	std::size_t getRadius() const
+	{
+		return radius;
+	}
+	std::size_t getSize() const
+	{
+		return radius * 2 + 1;
+	}
+	float operator[](std::size_t id) const
+	{
+		return data[id];
+	}
+	float& operator[](std::size_t id)
+	{
+		return data[id];
+	}
+};
